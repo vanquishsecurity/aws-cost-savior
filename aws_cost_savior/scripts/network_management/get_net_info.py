e5e613611829eb337fb73ee95f459ee04034e22a
vanquishsecurity <vanquish.security@gmail.com>
2022-12-13 17:20:05 -0500

    Alpha 0.1

---
+++ aws_cost_savior/scripts/network_management/get_net_info.py
@@ -0,0 +1,16 @@
+#AWS Cost Saver
+##Contact vanquish.security@gmail.com
+##https://github.com/vanquishsecurity/aws-cost-savior
+##GNU General Public License v3.0
+#
+#
+#
+#Python script using AWS CLI v2 to get network information from an AWS VPC enviroment
+#
+#
+#Get EC2 Elastic IP addresses
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-addresses.html
+#
+#
+
+aws ec2
